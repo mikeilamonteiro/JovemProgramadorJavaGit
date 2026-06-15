a5b79be1b24aacf17f88ead6b014d0a6db2f56c3
mikeila62930716 <mikeila62930716@BUE0108D016.alunos.sc.senac.br>
2026-06-15 11:13:10 -0300

    commit inicial para github

---
+++ ProjetoIntgegracaoGitHub/src/Integracao/TesteIntegracao.java
@@ -0,0 +1,11 @@
+package Integracao;
+
+public class TesteIntegracao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("primeiro teste");
+
+	}
+
+}
